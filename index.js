74fb071d34ae3cc272cc1a4d498998ee49613a69
Olga1Chernova <107894331+Olga1Chernova@users.noreply.github.com>
2022-12-25 03:20:33 +0200

    upd

--- index.js
+++ index.js
@@ -4,14 +4,22 @@ const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', (socket) => {
-  console.log('a user connected');
+    console.log('a user connected');
+
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);
+  });
+    
+    socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
 });
 
 server.listen(PORT, () => {
